4b8d9fddcad0bb15ec0d1c97c44ceb73434b7134
stringpappy01 <jejeodunayo70@gmail.com>
2024-01-27 15:03:00 -0800

    printf

---
+++ _printf.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _printf -function that produce output according to a format
+ * @format: pointeer to variable in main.c
+ *Return 0
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0, char_print = 0;
+
+	
+	va_list folder;
+
+	if (format == NULL);
+		return (-1);
+	va_start(folder, format);
+	
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1)
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			else if(*format == 'c')
+			{
+				chr letter = va_arg(folder, int);
+					write(1, &letter, 1);
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(folder,char *);
+				if(str == NULL)
+				{
+					write(1, "NULL", )
+	va_end(count)
