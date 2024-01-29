9180d6cdab974120e59ddaaa69371afa1336cb9f
Mbaruku2003 <leonardjembe21@gmail.com>
2024-01-29 04:02:15 -0800

    %r" "

--- main.h
+++ main.h
@@ -5,7 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(const char *wow, ...);
 
 #endif
--- s.c
+++ s.c
@@ -1,54 +1,47 @@
-#include <unistd.h>
 #include "main.h"
+#include <unistd.h>
 /**
- * _printf -function that produce output according to a format
- * @format: pointeer to variable in main.c
- *Return 0
+ * _printf - print character with %c
+ *@format: argument passed
+ *Return: always 1
  */
 int _printf(const char *format, ...)
 {
 	int count = 0;
+	int i;
+	int length = 0;
 	va_list folder;
 
 	if (format == NULL || format[0] == '\0')
-	{
-	return (-1);
-	}
+		return (-1);
 	va_start(folder, format);
 	while (*format)
 	{
 		if (*format != '%')
-		{
 			write(1, format, 1);
-		}
 		else
 		{
 			format++;
 			if (*format == '\0')
-			break;
-			else if(*format == 'c')
-			{
-				char letter = va_arg(folder, int);
-				write (1, &letter, 1);
-			}
+				break;
 			else if (*format == 's')
 			{
 				char *str = va_arg(folder, char *);
-				if (str == NULL)
-				{
-					write(1, "NULL", 5);
-					count++;
-				}
-			}
-			else if (*format == '%')
-			{
-				write (1, format, 1);
-				count++;
-			}
 
+
+				if (str == NULL)
+					write(1, "NULL", 5);
+				else
+				for (i = 0; str[i]; i++)
+					while (*str && length++)
+					{
+						write(1, str++, 1);
+						count++;
+					}
+			}
 		}
 		format++;
-}
-va_end(folder);
-return (count);
+	}
+	va_end(folder);
+	return (count);
 }
