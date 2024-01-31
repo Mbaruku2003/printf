4c9594ff789b6b9cef4b444703def7d37aa7a4d4
stringpappy01 <jejeodunayo70@gmail.com>
2024-01-31 13:27:16 -0800

    printf first task

--- main.h
+++ main.h
@@ -5,13 +5,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <string.h>
 
-int leo(const char *format, ...);
+
+int string_print(va_list folder);
+int print_char(va_list folder);
 int _putchar(char c);
-int s(const char *format, ...);
-int r(const char *format, ...);
-int c(const char *format, ...);
 int _printf(const char *format, ...);
 
 #endif
