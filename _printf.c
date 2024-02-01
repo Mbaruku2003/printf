76e99a3e7ffd3c60b2b6ff07d773fe1e1698e4a6
Mbaruku2003 <leonardjembe21@gmail.com>
2024-02-01 10:48:57 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,57 +1,85 @@
 #include <unistd.h>
+#include <stdarg.h>
 #include "main.h"
+#include <stddef.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * _printf -function that produce output according to a format
  * @format: pointeer to variable in main.c
  *Return: 0
  */
-int _printf(const char *format, ...);
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list folder;
+
+	int g_strint_cnt, _str_length;
+	char per_di[20];
+	va_list m_list;
+	g_strint_cnt = 0;
 
 	if (format == NULL || format[0] == '\0')
-	{
-	return (-1);
-	}
-	va_start(folder, format);
+		return -1;
+	va_start(m_list, format);
 	while (*format)
 	{
-		if (*format != '%')
+	if (*format != '%')
+	{
+		write (1, format, 1);
+		g_strint_cnt++;
+	}
+	else
+	{
+		format++;
+		if (*format == '\0')
+			break;
+		if (*format == '%')
 		{
-			write(1, format, 1);
+			g_strint_cnt++;
+		}
+		else if (*format == 'c')
+		{
+		char chars = va_arg(m_list, int);
+		write (1, &chars, 1);
+		g_strint_cnt++;
+	}
+	else if (*format == 's')
+	{
+		char *sttp = va_arg(m_list, char *);
+		if (sttp == NULL)
+		{
+			write (1, "(null)", 6);
+			g_strint_cnt += 6;
 		}
 		else
 		{
-			format++;
-			if (*format == '\0')
-			break;
-			else if (*format == 'c')
+			_str_length = 0;
+			while (sttp[_str_length] != '\0')
 			{
-				char letter = va_arg(folder, int);
-
-				write(1, &letter, 1);
+				write (1, &sttp[_str_length], 1);
+				_str_length++;
 			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(folder, char *);
-
-				if (str == NULL)
-				{
-					write(1, "NULL", 5);
-					count++;
-				}
-			}
-			else if (*format == '%')
-			{
-				write(1, format, 1);
-				count++;
-			}
-
+			g_strint_cnt += _str_length;
 		}
-		format++;
+	}
+	else if (*format == 'd' || *format == 'i')
+	{
+		int d_num = va_arg(m_list, int);
+		snprintf(per_di, sizeof(per_di), "%d", d_num);
+		write (1, per_di, strlen(per_di));
+		g_strint_cnt += strlen(per_di);
+	}
+	else
+	{
+		char m_error[7];
+		int m_err_len = snprintf(m_error, sizeof(m_error)," %%c", *format);
+		write (1, m_error, m_err_len);
+		g_strint_cnt = -1;
+	}
 }
-va_end(folder);
-return (count);
+format++;
+}
+va_end(m_list);
+return (g_strint_cnt);
 }
